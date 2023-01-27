060ce85d0e2debfdcf19f7a5df18eb7125ef6b6a
Humberto Massa <humbertomassa@gmail.com>
2023-01-27 19:26:45 -0300

    missing directory from install

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 rm -vrf ~/.config/nvim ~/.cache/nvim ~/.local/share/nvim
 mkdir -p ~/.config/nvim
-cp -vax init.lua lua ~/.config/nvim
+cp -vax init.lua lua after ~/.config/nvim
 nvim --headless -R +'sleep 120'
